c12593f308583abac02a003675d6dae46e3b1c2f
iqthuc <quangthuc256@gmail.com>
2025-04-04 10:57:31 +0700

    fix mongo in github action config

--- internal/database/mongodb.go
+++ internal/database/mongodb.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,6 +13,8 @@ import (
 )
 
 func NewMongoDB() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
@@ -20,12 +23,12 @@ func NewMongoDB() (*mongo.Client, error) {
 
 	clientOptions := options.Client().ApplyURI(mongoURI)
 
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.Connect(ctx, clientOptions)
 
 	if err != nil {
 		return nil, fmt.Errorf("Cannot connect to mongodb: %w", err)
 	}
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		return nil, fmt.Errorf("Cannot ping to mongodb: %w", err)
 	}
 	log.Println("connect to mongodb success")
